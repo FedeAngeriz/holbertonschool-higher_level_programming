d973b7de207b3e28d8e719bbfebbf21ea9e86103
FedeAngeriz <10086@holbertonstudents.com>
2025-01-25 20:25:44 -0300

    update files

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -2,4 +2,3 @@
 def print_matrix_integer(matrix=[[]]):
     for fila in matrix:
         print(" ".join("{:d}".format(elemento) for elemento in fila))
-        
--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-    tuple_a = tuple_a + (0,) * (len(tuple_b) - len(tuple_a))
-    tuple_b = tuple_b + (0,) * (len(tuple_a) - len(tuple_b))
-    return tuple(t1 + t2 for t1, t2 in zip(tuple_a, tuple_b))
+    lista = [0, 0]
+    for i in range(len(tuple_a)):
+        lista[i] += tuple_a[i]
+    for i in range(len(tuple_b)):
+        lista[i] += tuple_b[i]
+    return tuple(lista)
