c207a13b043a6f3dba7f24b97c61276fbe15526b
FedeAngeriz <10086@holbertonstudents.com>
2025-01-25 20:35:55 -0300

    update file

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -1,8 +1,12 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
     lista = [0, 0]
-    for i in range(len(tuple_a)):
+
+    
+
+
+    for i in range(min(2, len(tuple_a))):
         lista[i] += tuple_a[i]
-    for i in range(len(tuple_b)):
+    for i in range(min(2, len(tuple_b))):
         lista[i] += tuple_b[i]
     return tuple(lista)
