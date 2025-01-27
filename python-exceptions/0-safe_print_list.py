5908b3b6d9036116b771603b85051962b0982bca
FedeAngeriz <10086@holbertonstudents.com>
2025-01-26 23:49:20 -0300

    update file

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -3,8 +3,9 @@ def safe_print_list(my_list=[], x=0):
     contador = 0
     for i in range(x):
         try:
-            print(my_list[i])
+            print(my_list[i], end="")
             contador += 1
         except IndexError:
-            break
+            pass
+    print()
     return contador
