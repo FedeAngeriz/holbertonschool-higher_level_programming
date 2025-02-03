7195afcd58ec1d00364cbe06103fbc1a529fc906
FedeAngeriz <10086@holbertonstudents.com>
2025-02-03 00:01:11 -0300

    update

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,8 +1,13 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
+
+    if not isinstance(roman_string, str):
+        return 0
+
     list_r = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     resultado = 0
     prev = 0
+
     for number_r in roman_string[::-1]:
         number = list_r[number_r]
 
