767fb996d3a07acd92a5c6199a4b2113440a951a
FedeAngeriz <10086@holbertonstudents.com>
2025-02-08 19:51:32 -0300

    update file

--- python-inheritance/10-square.py
+++ python-inheritance/10-square.py
@@ -15,11 +15,11 @@ Rectangle = __import__('9-rectangle').Rectangle
 class Square(Rectangle):
     """Se define un cuadrado"""
     def __init__(self, size):
-        super().__init__(size, size)
+        self.integer_validator("size", size)
         self.__size = size
 
     def __str__(self):
-        return f"[Square] {self._Rectangle__width}/{self._Rectangle__height}"
+        return f"[Square] {self.__size}/{self.__size}"
 
     def area(self):
         return self.__size * self.__size
