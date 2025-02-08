61b0d5fee0862b86f5823951186c1290d2efd461
FedeAngeriz <10086@holbertonstudents.com>
2025-02-08 18:45:25 -0300

    update file

--- python-inheritance/10-square.py
+++ python-inheritance/10-square.py
@@ -13,35 +13,16 @@ class BaseGeometry:
         if value <= 0:
             raise ValueError(f"{name} must be greater than 0")
 
-
-"""Creamos clase Rectangle, que hereda BaseGeometry"""
-
-
-class Rectangle(BaseGeometry):
-    """Se define un rectangulo con alto y ancho validados"""
-    def __init__(self, width, height):
-        self.integer_validator("width", width)
-        self.integer_validator("height", height)
-        self.__width = width
-        self.__height = height
-
-    def area(self):
-        """Devuelve area de rectangulo"""
-        return self.__width * self.__height
-
-    def __str__(self):
-        return f"[Rectangle] {self.__width}/{self.__height}"
-
+Rectangle = __import__('9-rectangle').Rectangle
 
 """Creamos clase Square, que hereda Rectangle"""
 
 
 class Square(Rectangle):
-    """Se define un cuadrado validado"""
+
     def __init__(self, size):
         self.integer_validator("size", size)
         self.__size = size
 
     def area(self):
-        """Devuelve area de cuadrado"""
         return self.__size * self.__size
