a0f8906e0fd922f008624cb7f0c7f6ef0b03e166
FedeAngeriz <10086@holbertonstudents.com>
2025-02-09 22:05:09 -0300

    update file 03

---
+++ python-abc/task_03_countediterator.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+class CountedIterator:
+    def __init__(self, iterador):
+        self.iterator = iter(iterador)
+        self.counter = 0
+
+    def get_count(self):
+        return self.counter
+
+    def __next__(self):
+        self.counter += 1
+        return next(self.iterator)
