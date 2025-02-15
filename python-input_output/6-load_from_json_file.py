89ca79e966a1852051c52f4b2b5b5e7531e3c39d
FedeAngeriz <10086@holbertonstudents.com>
2025-02-15 05:30:28 -0300

    update file

--- python-input_output/6-load_from_json_file.py
+++ python-input_output/6-load_from_json_file.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
+"""Se importa el modulo json"""
 
 import json
 
 
 def load_from_json_file(filename):
+    """Se crea un objeto a partir de un archivo json"""
     with open(filename, 'r', encoding="") as file:
         return json.load(file)
