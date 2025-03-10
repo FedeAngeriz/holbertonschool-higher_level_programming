8e0038d4456d630a255994d4b2e4abeecbf23306
FedeAngeriz <10086@holbertonstudents.com>
2025-03-10 02:01:48 -0300

    UPDATE final 1.0

--- SQL_introduction/16-no_link.sql
+++ SQL_introduction/16-no_link.sql
@@ -1,5 +1,5 @@
 -- lists all records of the table second_table
-select score, name
-from second_table
-where name is not null
-order by score desc;
+SELECT score, name
+FROM second_table
+WHERE name IS NOT NULL
+ORDER BY score DESC; 
