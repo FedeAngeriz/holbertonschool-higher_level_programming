d1275f9b0db2e51e24d680a6955bfe76b73e1173
FedeAngeriz <10086@holbertonstudents.com>
2025-03-16 15:38:14 -0300

    Update files

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,4 +1,5 @@
 -- Creacion de Usuario en Servidor
-CREATE USER 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_password';
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_password';
 GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
+
 FLUSH PRIVILEGES;
--- SQL_more_queries/3-force_name.sql
+++ SQL_more_queries/3-force_name.sql
@@ -1,5 +1,5 @@
 -- Creacion de tabla force_name
 CREATE TABLE IF NOT EXISTS force_name (
-    if INT,
+    id INT DEFAULT 1,
     name VARCHAR(256) NOT NULL
 );
