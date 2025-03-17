2e717d8ab039a259b06a8a68ec3efc11025e34c1
FedeAngeriz <10086@holbertonstudents.com>
2025-03-16 21:05:41 -0300

    update file

--- SQL_more_queries/11-genre_id_all_shows.sql
+++ SQL_more_queries/11-genre_id_all_shows.sql
@@ -1,6 +1,5 @@
 -- Enumerar todos los shows contenidos en la DB
-SELECT tv_shows.title, (
-    SELECT genre_id FROM tv_show_genres WHERE tv_show_genres.show_id = tv_shows.id LIMIT 1
-) AS genre_id
+SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_shows
-ORDER BY tv_shows.title ASC, genre_id ASC;
+LEFT OUTER JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+ORDER BY tv_shows.title, tv_show_genres.genre_id ASC;
--- SQL_more_queries/12-no_genre.sql
+++ SQL_more_queries/12-no_genre.sql
@@ -1,5 +1,6 @@
 -- Enumerar todos los shows en la DB sin generos
-SELECT tv_shows.title, NULL AS genre_id
+SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_shows
-WHERE tv_shows.id NOT IN (SELECT show_id FROM tv_shows_genres)
-ORDER BY tv_shows.title ASC, genre_id ASC;
+LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+WHERE tv_show_genres.genre_id IS NULL
+ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
